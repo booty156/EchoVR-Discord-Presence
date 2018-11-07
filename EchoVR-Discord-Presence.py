eeb216824de2e8fab6605341eb48fd018cdc6d17
Ajedi32 <ajedi32.web@gmail.com>
2018-11-06 19:14:37 -0600

    Use echovr-api package
    Simplifies the code somewhat, and fixes inaccurate information being displayed
    when the game has a team with no players.

--- EchoVR-Discord-Presence.py
+++ EchoVR-Discord-Presence.py
@@ -1,7 +1,6 @@
 from pypresence import Presence
 import time
-import requests
-import json
+import echovr_api
 
 epoch  = round(time.time())
 
@@ -23,36 +22,22 @@ GAME_STATUS_DESCRIPTIONS = {
 }
 
 while True:
-    blue_team_size = 0
-    orange_team_size = 0
     try:
-        response = requests.get('http://127.0.0.1/session')
-        response_text = response.text.rstrip('\0')
-        response_object = json.loads(response_text)
+        game_state = echovr_api.fetch_state()
 
-        clock = response_object['game_clock_display']
+        clock = game_state.game_clock_display
+
         print(clock)
+        print(game_state.game_status)
 
-        game_status = response_object['game_status']
-        print(game_status)
-
-        game_status_description = GAME_STATUS_DESCRIPTIONS.get(game_status, '')
-        if game_status in ['round_over', 'post_match']:
+        game_status_description = GAME_STATUS_DESCRIPTIONS.get(game_state.game_status, '')
+        if game_state.game_status in ['round_over', 'post_match']:
             clock = '00:00.00'
 
-        try:
-            for team in response_object['teams']:
-                if team['team'] == 'BLUE TEAM':
-                    blue_team_size = len(team['players'])
-                    blue_points = team['stats']['points']
-                else:
-                    orange_team_size = len(team['players'])
-                    orange_points = team['stats']['points']
-        except KeyError:
-            blue_team_size = 0
-            orange_team_size = 0
-            orange_points = 0
-            blue_points = 0
+        blue_team_size = len(game_state.blue_team.players)
+        orange_team_size = len(game_state.orange_team.players)
+        blue_points = game_state.blue_team.score
+        orange_points = game_state.orange_team.score
 
         RPC.update(
             details = f"In Arena | {orange_points} - {blue_points} | {clock}",
@@ -70,5 +55,5 @@ while True:
             large_text = 'Echo Arena',
             small_image = 'echosmall',
             small_text = 'Echo VR',
-            start = epoch)       
+            start = epoch)
     time.sleep(10)
